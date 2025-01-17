e7e3f2c962e678d83e368ee199c9a472ad6195cb
hirona <hiro.98v7@gmail.com>
2025-01-17 21:52:42 +0900

    feat: マウスユーティリティを追加
     - マウスの座標を取得する機能を実装

---
+++ Assets/uDesktopMascot/Scripts/Utility/MouseUtility.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+namespace uDesktopMascot
+{
+    /// <summary>
+    /// Windows上のマウスに関するユーティリティクラス
+    /// </summary>
+    public static class MouseUtility
+    {
+        /// <summary>
+        /// マウスポインタのポジション格納用(WindowsAPI呼び出しで使用)
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out POINT lpPoint);
+
+        /// <summary>
+        /// マウスポインタの位置を取得する
+        /// </summary>
+        public static Vector2 GetCursorPosition()
+        {
+            if (!GetCursorPos(out POINT point))
+            {
+                return Vector2.zero;
+            }
+
+            return new Vector2(point.X, point.Y);
+        }
+    }
+}
