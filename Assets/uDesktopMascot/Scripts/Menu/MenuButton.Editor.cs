5b7097103f6bf27d2a4b0bd1d6236c83d5c88bea
yousan <ka1357amnbpdr@gmail.com>
2025-01-26 12:44:18 +0900

    fix: menuのuiの修正

---
+++ Assets/uDesktopMascot/Scripts/Menu/MenuButton.Editor.cs
@@ -0,0 +1,26 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine.UI;
+
+namespace uDesktopMascot
+{
+    /// <summary>
+    ///   メニューボタンクラス Editor 拡張
+    /// </summary>
+    public partial class MenuButton
+    {
+        private void OnValidate()
+        {
+            if (_button == null)
+            {
+                _button = GetComponent<Button>();
+            }
+            if (_button != null && buttonText != null)
+            {
+                buttonText.enabled = _button.interactable;
+                EditorUtility.SetDirty(buttonText);
+            }
+        }
+    }
+}
+#endif
